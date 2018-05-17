ee379009642cdd34ad92cdaa9214a7a83412c1b0
itchypupskit <matthiasjirmann@gmail.com>
2018-05-17 23:17:56 +0100

    update

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -117,5 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspatch(__file__)
+    )
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR + '/www/static',
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
